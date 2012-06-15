a576cc031d225d6e00de2762209469cc9aff2411
Nishanth Menon <nm@ti.com>
2012-06-15 12:35:05 +0300

    ARM: OMAP4: DPLL: DO NOT do DLL reset when changing M2/frequpdate
    PRCM programming model in OMAP4 and OMAP5 require that DLL reset
    NOT to be done as part of frequpdate(EMIF) and when M2 divider is
    changed.
    
    Change-Id: I486e1381fa8b86ef4b903e778b8638d553800dfe
    Signed-off-by: Eugen Mandrenko <ievgen.mandrenko@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>
    
    Conflicts:
    	arch/arm/mach-omap2/dpll44xx.c

--- arch/arm/mach-omap2/dpll44xx.c
+++ arch/arm/mach-omap2/dpll44xx.c
@@ -130,6 +130,7 @@ int omap4_core_dpll_m2_set_rate(struct clk *clk, unsigned long rate)
 	shadow_freq_cfg1 = (1 << OMAP4430_DLL_RESET_SHIFT) |
 			(new_div << OMAP4430_DPLL_CORE_M2_DIV_SHIFT) |
 			(DPLL_LOCKED << OMAP4430_DPLL_CORE_DPLL_EN_SHIFT) |
+			(0 << OMAP4430_DLL_RESET_SHIFT) |
 			(1 << OMAP4430_FREQ_UPDATE_SHIFT);
 	shadow_freq_cfg1 &= ~OMAP4430_DLL_OVERRIDE_MASK;
 	__raw_writel(shadow_freq_cfg1, OMAP4430_CM_SHADOW_FREQ_CONFIG1);
@@ -195,8 +196,8 @@ int omap4_prcm_freq_update(void)
 	 * - FREQ_UPDATE=1 (to start HW sequence)
 	 */
 	shadow_freq_cfg1 = __raw_readl(OMAP4430_CM_SHADOW_FREQ_CONFIG1);
-	shadow_freq_cfg1 |= (1 << OMAP4430_DLL_RESET_SHIFT) |
-			   (1 << OMAP4430_FREQ_UPDATE_SHIFT);
+	shadow_freq_cfg1 &= ~(1 << OMAP4430_DLL_RESET_SHIFT);
+	shadow_freq_cfg1 |= (1 << OMAP4430_FREQ_UPDATE_SHIFT);
 	shadow_freq_cfg1 &= ~OMAP4430_DLL_OVERRIDE_MASK;
 	__raw_writel(shadow_freq_cfg1, OMAP4430_CM_SHADOW_FREQ_CONFIG1);
 
