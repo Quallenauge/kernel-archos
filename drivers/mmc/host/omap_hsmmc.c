154d621500e46f274f91a07e57ddce625d46b254
Jianpeng Ma <majianpeng@gmail.com>
2013-10-21 00:25:20 +0530

    mmc: omap_hsmmc: fix timeout for cmd and data soft reset
    With HSMMC_HAS_UPDATED_RESET reset of cmd/data (SRC/SRD) can be to
    quick and can be missed resulting in wait for software timeout.
    With cpu_relax timeout can be long and unpredictable. Use udelay
    instead for timeout implementation.
    
    Reported-by: Yuzheng Ma <mayuzheng@kedacom.com>
    Tested-by: Yuzheng Ma <mayuzheng@kedacom.com>
    Reviewed-by: Hein Tibosch <hein_tibosch@yahoo.es>
    Signed-off-by: Jianpeng Ma <majianpeng@gmail.com>
    Signed-off-by: Balaji T K <balajitk@ti.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -159,7 +159,8 @@
 #define OMAP_MMC4_DEVID		3
 #define OMAP_MMC5_DEVID		4
 
-#define MMC_TIMEOUT_MS		20
+#define MMC_TIMEOUT_MS         20              /* 20 mSec */
+#define MMC_TIMEOUT_US         20000           /* 20000 micro Sec */
 #define OMAP_MMC_MASTER_CLOCK	96000000
 #ifdef CONFIG_OMAP4_DPLL_CASCADING
 #define OMAP_MMC_DPLL_CLOCK	49152000
@@ -1130,8 +1131,7 @@ static inline void omap_hsmmc_reset_controller_fsm(struct omap_hsmmc_host *host,
 						   unsigned long bit)
 {
 	unsigned long i = 0;
-	unsigned long limit = (loops_per_jiffy *
-				msecs_to_jiffies(MMC_TIMEOUT_MS));
+	unsigned long limit = MMC_TIMEOUT_US;
 
 	OMAP_HSMMC_WRITE(host->base, SYSCTL,
 			 OMAP_HSMMC_READ(host->base, SYSCTL) | bit);
@@ -1143,13 +1143,13 @@ static inline void omap_hsmmc_reset_controller_fsm(struct omap_hsmmc_host *host,
 	if (mmc_slot(host).features & HSMMC_HAS_UPDATED_RESET) {
 		while ((!(OMAP_HSMMC_READ(host->base, SYSCTL) & bit))
 					&& (i++ < limit))
-			cpu_relax();
+			udelay(1);
 	}
 	i = 0;
 
 	while ((OMAP_HSMMC_READ(host->base, SYSCTL) & bit) &&
 		(i++ < limit))
-		cpu_relax();
+		udelay(1);
 
 	if (OMAP_HSMMC_READ(host->base, SYSCTL) & bit)
 		dev_err(mmc_dev(host->mmc),
