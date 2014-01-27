cc8f1fc60303bc7977d1610b8beba4d0d7b463e6
myfluxi <linflux@arcor.de>
2014-01-27 01:21:10 +0100

    arm: vfpmodule: Fix warning procfs vfp_bounce reporting failed
    Creation of procfs cpu/vfp_bounce fails because we're initialized too early. Fix
    this by creating it on rootfs_initcall as before the NEON patches.
    
    <6>[    0.130770] VFP support v0.3: implementor 51 architecture 64 part 6f varia
    nt 2 rev 0
    <4>[    0.130795] ------------[ cut here ]------------
    <4>[    0.130813] WARNING: at fs/proc/generic.c:323 __xlate_proc_name+0xac/0xcc(
    )
    <4>[    0.130822] name 'cpu/vfp_bounce'
    <4>[    0.130855] [<c010e26c>] (unwind_backtrace+0x0/0x144) from [<c0a20f58>] (d
    ump_stack+0x20/0x24)
    <4>[    0.130879] [<c0a20f58>] (dump_stack+0x20/0x24) from [<c019b670>] (warn_sl
    owpath_common+0x58/0x70)
    <4>[    0.130899] [<c019b670>] (warn_slowpath_common+0x58/0x70) from [<c019b704>
    ] (warn_slowpath_fmt+0x40/0x48)
    <4>[    0.130919] [<c019b704>] (warn_slowpath_fmt+0x40/0x48) from [<c02c2ad8>] (
    __xlate_proc_name+0xac/0xcc)
    <4>[    0.130938] [<c02c2ad8>] (__xlate_proc_name+0xac/0xcc) from [<c02c2b50>] (
    __proc_create+0x58/0x100)
    <4>[    0.130956] [<c02c2b50>] (__proc_create+0x58/0x100) from [<c02c2ed0>] (pro
    c_create_data+0x5c/0xc0)
    <4>[    0.130979] [<c02c2ed0>] (proc_create_data+0x5c/0xc0) from [<c0f03484>] (v
    fp_init+0x19c/0x200)
    <4>[    0.131000] [<c0f03484>] (vfp_init+0x19c/0x200) from [<c0f00c98>] (do_one_
    initcall+0x98/0x168)
    <4>[    0.131020] [<c0f00c98>] (do_one_initcall+0x98/0x168) from [<c0f00e60>] (k
    ernel_init+0xf8/0x1b4)
    <4>[    0.131043] [<c0f00e60>] (kernel_init+0xf8/0x1b4) from [<c01081a0>] (kerne
    l_thread_exit+0x0/0x8)
    <4>[    0.131076] ---[ end trace ea6d9a9b5e947151 ]---
    <3>[    0.131086] Failed to create procfs node for VFP bounce reporting
    
    Conflicts:
    	arch/arm/vfp/vfpmodule.c

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -23,6 +23,7 @@
 #include <linux/user.h>
 #include <linux/proc_fs.h>
 #include <linux/hardirq.h>
+#include <linux/seq_file.h>
 
 #include <asm/cputype.h>
 #include <asm/thread_notify.h>
@@ -66,6 +67,11 @@ union vfp_state *vfp_current_hw_state[NR_CPUS];
  * After startup, holds VFP architecture
  */
 unsigned int VFP_arch;
+
+/*
+* Used for reporting emulation statistics via /proc
+*/
+static atomic64_t vfp_bounce_count = ATOMIC64_INIT(0);
 
 /*
  * Per-thread VFP initialization.
@@ -318,6 +324,7 @@ void VFP_bounce(u32 trigger, u32 fpexc, struct pt_regs *regs)
 	u32 fpscr, orig_fpscr, fpsid, exceptions;
 
 	pr_debug("VFP: bounce: trigger %08x fpexc %08x\n", trigger, fpexc);
+	atomic64_inc(&vfp_bounce_count);
 
 	/*
 	 * At this point, FPEXC can have the following configuration:
@@ -563,6 +570,27 @@ static int vfp_hotplug(struct notifier_block *b, unsigned long action,
 	return NOTIFY_OK;
 }
 
+
+#ifdef CONFIG_PROC_FS
+static int vfp_bounce_show(struct seq_file *m, void *v)
+{
+        seq_printf(m, "%llu\n", atomic64_read(&vfp_bounce_count));
+        return 0;
+}
+
+static int vfp_bounce_open(struct inode *inode, struct file *file)
+{
+        return single_open(file, vfp_bounce_show, NULL);
+}
+
+static const struct file_operations vfp_bounce_fops = {
+        .open           = vfp_bounce_open,
+        .read           = seq_read,
+        .llseek         = seq_lseek,
+        .release        = single_release,
+};
+#endif
+
 void vfp_kmode_exception(void)
 {
 	/*
@@ -707,7 +735,22 @@ static int __init vfp_init(void)
 		}
 #endif
 	}
+
+	return 0;
+}
+
+static int __init vfp_rootfs_init(void)
+{
+#ifdef CONFIG_PROC_FS
+	static struct proc_dir_entry *procfs_entry;
+
+	procfs_entry = proc_create("cpu/vfp_bounce", S_IRUGO, NULL,
+			&vfp_bounce_fops);
+	if (!procfs_entry)
+		pr_err("Failed to create procfs node for VFP bounce reporting\n");
+#endif
 	return 0;
 }
 
 core_initcall(vfp_init);
+rootfs_initcall(vfp_rootfs_init);
